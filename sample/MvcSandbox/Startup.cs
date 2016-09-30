fa68eba72be42e6c1bb5576c30eafbf0462e1ec9
Andrei Tserakhau <andrei_tserakhau@epam.com>
2016-09-30 12:57:51 +0300

    refactor(sample): clean-up code in sample app

--- sample/MvcSandbox/Startup.cs
+++ sample/MvcSandbox/Startup.cs
@@ -1,54 +1,53 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-
 namespace MvcSandbox
 {
+	#region usings
+
+	using System.IO;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Logging;
 	using MvcDeviceDetector;
 	using MvcDeviceDetector.Abstractions;
-    using MvcDeviceDetector.Preference;
+	using MvcDeviceDetector.Preference;
+
+	#endregion
 
 	public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddTransient<ISitePreferenceRepository, SitePreferenceRepository>();
-            services.AddDeviceSwitcher<UrlSwitcher>();
-        }
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			services.AddTransient<ISitePreferenceRepository, SitePreferenceRepository>();
+			services.AddDeviceSwitcher<UrlSwitcher>();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseStaticFiles();
-            loggerFactory.AddConsole();
-            app.UseMvc(routes =>
-            {
-                routes.MapDeviceSwitcher();
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+		{
+			app.UseDeveloperExceptionPage();
+			app.UseStaticFiles();
+			loggerFactory.AddConsole();
+			app.UseMvc(routes =>
+			{
+				routes.MapDeviceSwitcher();
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
 
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseKestrel()
-                .UseStartup<Startup>()
-                .Build();
+		public static void Main(string[] args)
+		{
+			var host = new WebHostBuilder()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				.UseIISIntegration()
+				.UseKestrel()
+				.UseStartup<Startup>()
+				.Build();
 
-            host.Run();
-        }
-    }
+			host.Run();
+		}
+	}
 }
-
